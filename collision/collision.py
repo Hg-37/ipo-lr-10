053a4d876b3181d42afa4e401f356d1188e34610
Влад <yakubvladislav24@gmail.com>
2025-12-11 11:27:03 +0300

    Task 4 Complete

--- collision/collision.py
+++ collision/collision.py
@@ -5,6 +5,8 @@ def isCorrectRect(coords):
     x2, y2 = right_top
 
     return x1 < x2 and y1 < y2
+class RectCorrectError(Exception):
+     pass
 def isCollisionRect(rectangles):
     def is_valid_rect(rect_points, rect_number):
         (x1, y1), (x2, y2) = rect_points
@@ -33,5 +35,31 @@ def isCollisionRect(rectangles):
                    y4 <= y1)    
 
     return not not_collide
+def intersectionAreaRect(rect1, rect2):
+    try:
+        isCorrectRect(rect1, 1)
+    except RectCorrectError as e:
+        raise RectCorrectError("1й прямоугольник некоректный") from e
+    
+    try:
+        isCorrectRect(rect2, 2)
+    except RectCorrectError as e:
+        raise RectCorrectError("2й прямоугольник некоректный") from e
+    
+    (x1, y1), (x2, y2) = rect1
+    (x3, y3), (x4, y4) = rect2
+    
+    left = max(x1, x3)
+    right = min(x2, x4)
+    bottom = max(y1, y3)
+    top = min(y2, y4)
+    
+    if left < right and bottom < top:
+        width = right - left
+        height = top - bottom
+        return width * height
+    else:
+        return 0
+
 
 
